efd42160c909c0bbef50e35114fcdd7e63872589
lorenzafiorillo31-hub <lorenzafiorillo31@gmail.com>
2025-10-27 13:08:22 +0100

    Add files via upload

---
+++ python-fiorillo.py
@@ -0,0 +1,9 @@
+var = float (input("gimme 1 num: "))
+var2= float(input("gimme another: "))
+
+print("Sum: ", var + var2)
+
+var = float(input("gimme 1 num: "))
+var2= float(input("gimme another: "))
+
+print("Minus: ", var + var2)
