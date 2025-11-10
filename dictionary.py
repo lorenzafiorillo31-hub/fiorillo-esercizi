22de14a376caddc7dbf8a5f8d34949cbfa5a3600
lorenzafiorillo31-hub <lorenzafiorillo31@gmail.com>
2025-11-10 12:58:32 +0100

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+person = {"color": "purple"}
+print (person)
+print(len (person))
+person.keys ()
+person.values ()
+person.items ()
+person.get
+person.update({"letter":"P"})
+person.pop("letter")
+person.clear ()
